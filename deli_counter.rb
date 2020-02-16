8274f49eff42c35bf20b5496b32cfde02a075629
Melissa Sperry <sperry.mel@gmail.com>
2020-02-16 04:58:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+katz_deli = ["Moshe", "Fayge", "Rivki"]
 
 
 def line(array)
@@ -14,26 +15,38 @@ def line(array)
   end
 end
 
-def line_simple(array)
+line(katz_deli)
+
+def line_simple(array) 
   current_line = "The simple line is currently:"
   array.each.with_index(1) do |value, indexemus|
+  # "each.with_index" is the method...must use "index"
     current_line << " #{indexemus}. #{value},"
+    # "indexemus" is used to illustrate this variable can be different from "index"
   end
   puts current_line
 end
 
+line_simple(katz_deli)
+#[:foo, :bar, :baz].each.with_index(1) do |value, index|
+#    puts "#{index}: #{value}"
+#end
+
+
+
 def take_a_number(line, new_person)
-  line.push(new_person)
+  line.push(new_person) # could say: "line << new_person"
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
 take_a_number(katz_deli, "Fyvish")
 
 def now_serving(line)
-  if line.length == 0
+  if line.length == 0 # could say: "if deli.empty?"
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}."
+    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
+    line.shift # this works in the IDE but no on repl.it
   end
 end
 
