648a5e6f8032d5a3eef9ed9aa1190d175a89031c
Melissa Sperry <sperry.mel@gmail.com>
2020-02-16 04:55:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,14 +24,14 @@ def line_simple(array)
 end
 
 def take_a_number(line, new_person)
-  line.push(new_person) 
+  line.push(new_person)
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
 take_a_number(katz_deli, "Fyvish")
 
 def now_serving(line)
-  if line.length == 0 
+  if line.length == 0
     puts"There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}." 
